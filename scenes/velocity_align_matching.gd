79b0ae57beceb812395373292c2f6afebd97942f
Jorge Correia <14-10254@usb.ve>
2024-10-09 23:05:55 -0400

    otros ajustes

--- scenes/velocity_align_matching.gd
+++ scenes/velocity_align_matching.gd
@@ -1,44 +1,50 @@
 extends Node2D
 
-var maxAngularAcceleration : float  = 300
-var maxRotation : float = 50
-var targetRadius : float = 0.2
+var maxAngularAcceleration : float  = 3
+var maxRotation : float = 5
+var targetRadius : float = 0.02
 var slowRadius : float = 0.01
 var timeToTarget : float = 0.1
-var target
-var character
-var explicitTarget
+var maxAcceleration : float = 400
+# Called when the node enters the scene tree for the first time
+var velocity : Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+#	print(mapToRange(PI))
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	target = get_parent().get_parent().get_node("Node2D")
-	character = get_node(".")
+	var target = get_parent().get_parent().get_node("Node2D")
+	var character = get_node(".")
 	update(delta,character,target)
 	
 func update(delta,character,target) -> void:
-	var rot = getFaceSteering(target,character)
+	var rot = getSteering(target,character)
 	if rot == null:
 		rot = 0
 	character.rotation += rot * delta
+	#print("aafs",character.velocity)
+	var charvel = getSteeringVelocity(target,character)
+	character.velocity = charvel
+	character.position += charvel * delta
+	
 	
 func mapToRange(angle: float):
 	return fmod((angle + PI),(2 * PI)) - PI
-	
-func getFaceSteering(target,character):
-	var direction = target.position - character.position
-	if direction.length() == 0:
-		return null
-	
-	var align_target : Node2D = target
-	character.set_rotation(atan2(-direction.x,direction.y))
-	return getAlignSteering(align_target,character)
 
+func getSteeringVelocity(target,character):
+	var char_velocity = target.velocity - character.velocity
+	char_velocity /= timeToTarget
 	
-func getAlignSteering(target,character):
+	if char_velocity.length() > maxAcceleration:
+		char_velocity = char_velocity.normalized()
+		char_velocity *= maxAcceleration
+		
+	return char_velocity
+
+func getSteering(target,character):
 	var char_rotation
 	char_rotation = target.get_rotation() - character.get_rotation()
 	var targetRotation
--- scripts/node_2d.gd
+++ scripts/node_2d.gd
@@ -1,6 +1,6 @@
 extends Node2D
 # Position y rotation vienen definidos como vector y flotante respectivamente
-var speed : int = 300
+var speed : int = 400
 var orientation : float
 var angular : float
 var velocity : Vector2
@@ -26,20 +26,24 @@ func update(delta: float):
 		rotation -= PI/28
 	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
+		rotation = PI
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
+		rotation = 0
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
+		rotation = PI/2
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
+		rotation = 3*PI/2
 	#rotation = angular * delta	
-	orientation += rotation
+	
 	
 	if velocity.length() > 0:
 		#velocity.x += orientation
 		velocity = velocity.normalized() * speed
-		
 	position += velocity * delta
+	#rotation = -newOrientation(rotation,velocity)
 	
 
 func newOrientation(current: float, velocity: Vector2) -> float:
